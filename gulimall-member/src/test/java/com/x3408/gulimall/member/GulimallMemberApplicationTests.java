064bf8c7dda312cbfa310dc700b69694a85e66a9
许晨 <x3408@hotmail.com>
2020-05-19 11:17:25 +0800

    init

---
+++ gulimall-member/src/test/java/com/x3408/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x3408.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
